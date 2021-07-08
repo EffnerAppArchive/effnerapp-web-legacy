26c905144208c24b8e8aee4026c704b3656938e5
Sebi <sebim254@gmail.com>
2021-07-08 19:21:03 +0200

    update titles

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'app.effner.web',
-  appName: 'effnerapp@web',
+  appName: 'EffnerApp',
   webDir: 'dist',
   bundledWebRuntime: false
 };
