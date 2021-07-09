12d3cda2cda781c2f6d6431c5c2ad6627845294c
Sebi <sebim254@gmail.com>
2021-07-09 17:26:24 +0200

    added PWA

--- src/main.ts
+++ src/main.ts
@@ -25,6 +25,8 @@ import '@ionic/vue/css/display.css';
 /* Theme variables */
 import './theme/variables.css';
 
+import './registerServiceWorker'
+
 loadStorage().then(() => {
     const app = createApp(App)
         .use(IonicVue)
