403646d5fc7fdd0a420fb37cf6b874d212e1179e
Sebi <sebim254@gmail.com>
2021-07-10 13:44:44 +0200

    add SubstitutionItem.vue

---
+++ src/components/SubstitutionItem.vue
@@ -0,0 +1,20 @@
+<template>
+<div></div>
+</template>
+
+<script lang="ts">
+import {defineComponent} from "vue";
+
+export default defineComponent({
+  name: 'SubstitutionItem',
+  props: {
+    head: String,
+    body: String,
+    time: String
+  },
+})
+</script>
+
+<style scoped>
+
+</style>
