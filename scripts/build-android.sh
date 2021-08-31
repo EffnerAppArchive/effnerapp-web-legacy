a508f5be014aa9cd92c436cffd8e3e9f29b80fa8
Sebi <sebim254@gmail.com>
2021-08-31 11:36:08 +0200

    add android

--- scripts/build-android.sh
+++ scripts/build-android.sh
@@ -4,4 +4,4 @@ npx cap copy android
 npx cap sync
 
 # copy pre-generated MainActivity.java to android source for dark-mode functionality
-yes | cp -rf scripts/.src/MainActivity.java android/app/src/main/java/de/effnerapp/effner/
+#yes | cp -rf scripts/.src/MainActivity.java android/app/src/main/java/de/effnerapp/effner/
