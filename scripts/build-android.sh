0c0b590557eea497c2f772ddc6bd9adceb0d8b32
Sebi <sebim254@gmail.com>
2021-08-31 04:01:15 +0200

    Update build-android.sh

--- scripts/build-android.sh
+++ scripts/build-android.sh
@@ -1,6 +1,6 @@
-ionic build
-ionic cap copy android
-ionic cap sync
+npx ionic build
+npx cap copy android
+npx cap sync
 
 # copy pre-generated MainActivity.java to android source for dark-mode functionality
 yes | cp -rf scripts/.src/MainActivity.java android/app/src/main/java/de/effnerapp/effner/
