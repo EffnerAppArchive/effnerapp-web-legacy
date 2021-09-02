f7c86401b62d001fd00bdc4758e1495e25dd53f2
Luuuuuis <45736619+Luuuuuis@users.noreply.github.com>
2021-09-02 22:27:37 +0200

    fixed splashscreen on android

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,16 +1,15 @@
-import { CapacitorConfig } from '@capacitor/cli';
+import {CapacitorConfig} from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'de.effnerapp.effner',
-  appName: 'EffnerApp',
-  webDir: 'dist',
-  bundledWebRuntime: false,
-  plugins: {
-    PushNotifications: {
-      presentationOptions: ['badge', 'sound', 'alert']
+    appId: 'de.effnerapp.effner',
+    appName: 'EffnerApp',
+    webDir: 'dist',
+    bundledWebRuntime: false,
+    plugins: {
+        PushNotifications: {
+            presentationOptions: ['badge', 'sound', 'alert']
+        }
     }
-  }
-
 };
 
 export default config;
