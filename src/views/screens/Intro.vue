9c3e759e657cb8901a05652cb1932955ba4673dd
Sebi <sebim254@gmail.com>
2021-09-08 01:50:48 +0200

    added Timetable.vue

--- src/views/screens/Intro.vue
+++ src/views/screens/Intro.vue
@@ -2,10 +2,12 @@
   <div></div>
 </template>
 
-<script>
-export default {
+<script lang="ts">
+import {defineComponent} from 'vue';
+
+export default defineComponent({
   name: 'Intro'
-};
+});
 </script>
 
 <style scoped>
