4a4619ebbeb8c79a93cd652565a906f1c6694067
Luuuuuis <45736619+Luuuuuis@users.noreply.github.com>
2021-09-19 01:38:38 +0200

    version 2.0.1

--- scripts/update-versions.sh
+++ scripts/update-versions.sh
@@ -2,14 +2,14 @@ versionFile='../release/versions.json'
 iOSFile='../ios/App/App.xcodeproj/project.pbxproj'
 androidFile='../android/app/build.gradle'
 
-versionName=$(jq '.versionName' $versionFile)
+versionName=$(jq '.versionName' $versionFile | sed 's/\"//g')
 versionCodeIos=$(jq '.versionCodes.iOS' $versionFile)
 versionCodeAndroid=$(jq '.versionCodes.android' $versionFile)
 
 echo "$versionName", "$versionCodeIos", "$versionCodeAndroid"
 
 # Replace values in Android build.gradle
-sed -i'.backup' -e "s/##VERSIONNAME##/$versionName/g" $androidFile
+sed -i'.backup' -e "s/\"##VERSIONNAME##\"/$versionName/g" $androidFile
 sed -i'.backup' -e "s/696969/$versionCodeAndroid/g" $androidFile
 cat $androidFile
 
