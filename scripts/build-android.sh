66b4742fa4a259d9e5ddf925ec759a0648cf8525
Luuuuuis <45736619+Luuuuuis@users.noreply.github.com>
2021-11-15 19:12:19 +0100

    reverted push-notifications back to 1.0.4

--- scripts/build-android.sh
+++ scripts/build-android.sh
@@ -1,6 +1,3 @@
 npx ionic build
 npx cap copy android
 npx cap sync
-
-# temporary fix for push notifications on android
-cat push-notifications/android/build.gradle > node_modules/@capacitor/push-notifications/android/build.gradle
