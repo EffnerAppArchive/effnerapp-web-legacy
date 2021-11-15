bece49b807e68095edcac8dbf1d4f55a602508a1
Luuuuuis <45736619+Luuuuuis@users.noreply.github.com>
2021-11-15 18:42:38 +0100

    fix for android

--- scripts/build-android.sh
+++ scripts/build-android.sh
@@ -1,3 +1,6 @@
 npx ionic build
 npx cap copy android
 npx cap sync
+
+# temporary fix for push notifications on android
+cat push-notifications/android/build.gradle > node_modules/@capacitor/push-notifications/android/build.gradle
